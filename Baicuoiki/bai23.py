212dd6dd51ccc53ea738935407026bfeeb4e6d13
KhanhTrang005 <158994675+KhanhTrang005@users.noreply.github.com>
2024-11-15 18:22:00 +0700

    Update bai23.py

--- Baicuoiki/bai23.py
+++ Baicuoiki/bai23.py
@@ -5,8 +5,8 @@ Created on Fri Nov  8 23:16:55 2024
 @author: Windows
 """
 def question_23(nums: list[int]) -> bool:
-    for i in range(len(nums)):
-        if nums.count(nums[i])>1:
+   for i in nums:
+        if nums.count(i) > 1:
             return True
     return False
     print( question_23([1,1,2,3,4,5]))            
